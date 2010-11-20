50e1389dd3a7238018bce1d7c65bcc5bd8e0602a
nf@wh3rd.net <nf@amchitka.local>
2010-11-20 11:47:18 +1100

    works

--- writer.go
+++ writer.go
@@ -12,63 +12,44 @@ type File struct {
 	SignificantBits uint16
 	Channels uint16
 	b bytes.Buffer
-	done bool
 }
 
-func (f *File) Write(data []byte) (int, os.Error) {
-	if f.b.Len() == 0 {
-		if err := writeFmt(f); err != nil {
-			return 0, err
-		}
-	}
-	if err := writeChunk(&f.b, "data", data); err != nil {
-		return 0, err
-	}
-	return 8+len(data), nil
+func (f *File) Write(b []byte) (int, os.Error) {
+	return f.b.Write(b)
 }
 
-func (f *File) WriteTo(w io.Writer) (n int64, err os.Error) {
+func (f *File) WriteData(w io.Writer, data []byte) (err os.Error) {
 	defer func() {
 		if e, ok := recover().(os.Error); ok {
 			err = e
 		}
 	}()
-	write(w, "RIFF")
-	n += 4
+	writeFmt(f)
+	writeChunk(&f.b, "data", data)
+	write(w, []byte("RIFF"))
 	write(w, uint32(f.b.Len()))
-	n += 4
-	write(w, "WAVE")
-	n += 4
+	write(w, []byte("WAVE"))
 	write(w, f.b.Bytes())
-	n += int64(f.b.Len())
 	return
 }
 
 func writeFmt(f *File) (err os.Error) {
-	defer func() {
-		if e, ok := recover().(os.Error); ok {
-			err = e
-		}
-	}()
 	var b bytes.Buffer
 	write(&b, uint16(1)) // uncompressed/PCM
 	write(&b, f.Channels)
 	write(&b, f.SampleRate)
 	write(&b, f.SampleRate*uint32(f.SignificantBits)) // bytes per second
+	write(&b, f.SignificantBits / 8 * f.Channels) // block align
 	write(&b, f.SignificantBits)
+	write(&b, uint16(0)) // extra format bytes
 	return writeChunk(&f.b, "fmt ", b.Bytes())
 }
 
 func writeChunk(w io.Writer, id string, data []byte) (err os.Error) {
 	if len(id) != 4 {
-		return os.NewError("invalid chunk id")
+		panic(os.NewError("invalid chunk id"))
 	}
-	defer func() {
-		if e, ok := recover().(os.Error); ok {
-			err = e
-		}
-	}()
-	write(w, id)
+	write(w, []byte(id))
 	write(w, uint32(len(data)))
 	write(w, data)
 	return
